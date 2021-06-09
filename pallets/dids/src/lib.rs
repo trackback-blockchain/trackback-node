292c28ed9d8674816ac2556db8a57e97f7bf9605
Gayan <gayan@track-back.co>
2021-06-10 11:40:18 +1200

    Configured rustfmt tool for formatting

--- pallets/dids/src/lib.rs
+++ pallets/dids/src/lib.rs
@@ -5,27 +5,28 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
     // File hash retrieves as a vector
     // TODO:This should be a CBOR.
     use sp_std::vec::Vec;
 
-    #[pallet::config] 
+    #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	#[pallet::storage]
-    pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+    #[pallet::storage]
+    pub(super) type Proofs<T: Config> =
+        StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
 
     #[pallet::event]
     #[pallet::metadata(T::AccountId = "AccountId")]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]   
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         ProofCreated(T::AccountId, Vec<u8>),
     }
@@ -33,19 +34,17 @@ pub mod pallet {
     #[pallet::error]
     pub enum Error<T> {
         ProofExists,
-    }  
+    }
 
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-	#[pallet::call]
-	impl<T:Config> Pallet<T> {
-
-		#[pallet::weight(0)]
-		pub fn create_proof(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
-
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(0)]
+        pub fn create_proof(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResultWithPostInfo {
             let sender = ensure_signed(origin)?;
-    
+
             ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofExists);
 
             let current_block = <frame_system::Module<T>>::block_number();
@@ -53,7 +52,7 @@ pub mod pallet {
             Proofs::<T>::insert(&proof, (&sender, current_block));
 
             Self::deposit_event(Event::ProofCreated(sender, proof));
-			Ok(().into())
-		}
-	}
+            Ok(().into())
+        }
+    }
 }
