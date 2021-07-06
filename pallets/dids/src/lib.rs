9677b92b466c1225fa31913f282e8b329b93e1b5
Gayan <gayan@track-back.co>
2021-07-06 17:07:40 +1200

    Initial DID identifier

--- pallets/dids/src/lib.rs
+++ pallets/dids/src/lib.rs
@@ -9,30 +9,25 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
-    use pallet_timestamp as timestamp;
-    use sp_runtime::{
-        offchain as oc,
-        offchain::{
-            storage::StorageValueRef,
-            storage_lock::{BlockAndTime, StorageLock, Time},
-        },
-        DispatchResult,
-    };
+    use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*,
+                        };
 
-    use frame_system::{
-        offchain::{
-            AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
-            SignedPayload, Signer, SigningTypes, SubmitTransaction,
-        },
-        pallet_prelude::*,
-    };
+    use sp_runtime::{offchain as oc, offchain::{
+        storage::StorageValueRef,
+        storage_lock::{BlockAndTime, StorageLock, Time},
+    }, DispatchResult, AccountId32};
+
+    use frame_system::{offchain::{
+        AppCrypto, CreateSignedTransaction, SendSignedTransaction, SendUnsignedTransaction,
+        SignedPayload, Signer, SigningTypes, SubmitTransaction,
+    }, pallet_prelude::*, Account};
 
     use sp_std::str;
     use sp_std::vec::Vec;
+    use codec::EncodeLike;
 
     #[pallet::config]
-    pub trait Config: frame_system::Config + pallet_timestamp::Config{
+    pub trait Config: frame_system::Config + pallet_timestamp::Config  + {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
 
@@ -45,10 +40,19 @@ pub mod pallet {
     pub(super) type DIDs<T: Config> =
         StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
 
+    /// Stores a DID document on chain
+    /// Key 1 -> AccountId + DIDDocumentHash
+    /// Key 2 -> Chain time
+    /// Value -> DID Document(hash) + BlockNumber
     #[pallet::storage]
-    #[pallet::getter(fn did_docs)]
-    pub(super) type DIDDocument<T: Config> =
-        StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+    #[pallet::getter(fn get_did_document)]
+    pub(super) type DIDDocument<T: Config> = StorageMap<
+        _,
+        Blake2_128Concat, (T::AccountId, Vec<u8>),
+        // Blake2_128Concat, (Vec<u8>, Vec<u8>),
+        (T::Moment,Vec<u8>, T::BlockNumber),
+        ValueQuery
+    >;
 
     #[pallet::event]
     #[pallet::metadata(T::AccountId = "AccountId")]
@@ -61,7 +65,6 @@ pub mod pallet {
     pub enum Error<T> {
         DIDExists,
     }
-
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
         fn offchain_worker(block_number: T::BlockNumber) {
@@ -72,8 +75,11 @@ pub mod pallet {
             );
         }
     }
-
+    // impl<T: Encode> EncodeLike<T> for T::AccountId {}
     // Functions that are callable from outside the runtime
+    // impl EncodeLike for Account<T>{
+    //
+    // }
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         // fn offchain_worker(block_number: OriginFor<T>::BlockNumber){
@@ -90,6 +96,8 @@ pub mod pallet {
             Ok(().into())
         }
 
+
+        /// Stores a DID document
         #[pallet::weight(0)]
         pub fn insert_did_document(
             origin: OriginFor<T>,
@@ -97,22 +105,29 @@ pub mod pallet {
             did_hex: Vec<u8>,
             did_hash: Vec<u8>,
         ) -> DispatchResultWithPostInfo {
-            let sender = ensure_signed(origin);
+            let origin_account = ensure_signed(origin);
+            let  p = origin_account.unwrap();
 
-            // let acc = match sender {
-            //     | Ok(v) => v,
-            //     | _ => Err(BadOrigin),
-            // };
-            // let did_str = match str::from_utf8(origin) {
-            //     | Ok(v) => v,
-            //     | Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            // };
 
             let block_number = <frame_system::Module<T>>::block_number();
-            let parent_hash = <frame_system::Module<T>>::parent_hash();
-            let block_hash = <frame_system::Module<T>>::block_hash(block_number);
+            let time = <pallet_timestamp::Module<T>>::get();
 
-            // type did_key = sender +
+
+            let acc = Self::get_did_document((p.clone(), did_hash.clone()));
+            ensure!(!DIDDocument::<T>::contains_key(
+                &(p.clone(), did_hash.clone())
+            ), Error::<T>::DIDExists);
+
+            // DO NOT PANIC!
+            let did_str = match str::from_utf8(&*did_document) {
+                | Ok(v) => v,
+                | Err(e) => "",
+            };
+
+            DIDDocument::<T>::insert(
+                (p.clone(), did_hash.clone()),
+                (time, did_document, block_number)
+            );
 
             Ok(().into())
         }
