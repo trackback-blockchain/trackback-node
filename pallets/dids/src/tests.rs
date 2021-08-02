8bd8082445a9b87be314b775545bd031844550a5
Gayan <gayan@track-back.co>
2021-08-02 21:52:43 +1200

    Tests

--- pallets/dids/src/tests.rs
+++ pallets/dids/src/tests.rs
@@ -2,8 +2,10 @@ use super::*;
 use crate::mock::new_test_ext;
 use crate::mock::Origin;
 
-use frame_support::{assert_ok};
+use frame_support::{assert_ok, assert_err};
+use frame_support::pallet_prelude::DispatchError;
 use crate::mock::DIDModule;
+
 #[test]
 fn create_vc() {
     let public_key = vec![
@@ -15,5 +17,20 @@ fn create_vc() {
         assert_ok!(
             DIDModule::create_vc_fingerprint(Origin::signed(1), public_key, hash, Some(true))
         );
+    });
+}
+
+#[test]
+fn create_vc_exists() {
+    let public_key = vec![
+        0, 1, 217, 200, 51, 244, 152, 125, 173, 92, 30, 224, 60, 141, 221, 44, 65, 132, 45, 94,
+        199, 150, 116, 108, 95, 18, 118, 246, 86, 167, 64, 132, 76,
+    ];
+    let hash = "Hash".as_bytes().to_vec();
+    new_test_ext().execute_with(||{
+        DIDModule::create_vc_fingerprint(Origin::signed(1), public_key.clone(), hash.clone(), Some(true));
+        assert_err!(
+            DIDModule::create_vc_fingerprint(Origin::signed(1), public_key, hash, Some(true)),
+            DispatchError::Module { index: 1, error: 4, message: Some("VerifiableCredentialExists") } );
     });
 }
